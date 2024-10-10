0515de38899f414a00592f5492185d2c98b28bf4
saumya-babblebots <saumya@babblebots.ai>
2024-10-10 18:17:43 +0530

    Create region_voices.py

---
+++ region_voices.py
@@ -0,0 +1,18 @@
+# Map regions to their corresponding voice IDs and other voice settings
+REGION_VOICES = {
+    "US": {
+        "model": "eleven_turbo_v2_5",
+        "voiceId": "cjVigY5qzO86Huf0OWal",  
+        "provider": "11labs",
+        "stability": 0.5,
+        "similarityBoost": 0.75
+    },
+    "India": {
+        #"model": "eleven_turbo_v2_5",
+        "voiceId": "en-IN-AnanyaNeural",  
+        "provider": "azure",
+        "speed": 1.12,
+        #"stability": 0.5,
+        #"similarityBoost": 0.75
+    }
+}
